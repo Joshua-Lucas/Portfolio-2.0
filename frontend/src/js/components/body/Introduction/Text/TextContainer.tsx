caa5055e06d6a727610c6eb4bdf74def99654ee4
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-18 18:55:37 -0400

    add logic to handle section selection

--- frontend/src/js/components/body/Introduction/Text/TextContainer.tsx
+++ frontend/src/js/components/body/Introduction/Text/TextContainer.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import IntroDetails from './IntroDetails'
+import TextDetails from './TextDetails'
+
+import {
+  Frontend,
+  Backend,
+  EssentailSkills,
+} from '../../../../utils/StaticData'
 
 //Styled Components
 const TextWrapper = styled.div`
@@ -11,9 +18,32 @@ const TextWrapper = styled.div`
 
 //React Component
 const TextContainer: React.FC = () => {
+  const [state, setState] = useState('Backend')
+
+  const setSection = () => {
+    if (state == 'Frontend') {
+      return Frontend
+    } else if (state == 'Backend') {
+      return Backend
+    } else {
+      return EssentailSkills
+    }
+  }
+
   return (
     <TextWrapper>
-      <IntroDetails />
+      {state == 'Intro' ? (
+        <IntroDetails />
+      ) : (
+        <TextDetails
+          title={setSection().title}
+          subtitle={setSection().subtitle}
+          sectionTitleOne={setSection().sectionTitleOne}
+          sectionItemsOne={setSection().sectionItemsOne}
+          sectionTitleTwo={setSection().sectionTitleTwo}
+          sectionItemsTwo={setSection().sectionItemsTwo}
+        />
+      )}
     </TextWrapper>
   )
 }
