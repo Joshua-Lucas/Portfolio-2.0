465cd603a3e2104b3b46462d80a1a070c94dbf2b
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-18 18:50:46 -0400

    update color comments with rgb numbers

--- frontend/src/js/utils/theme/Colors.ts
+++ frontend/src/js/utils/theme/Colors.ts
@@ -2,10 +2,10 @@ const colors = {
   light: '#fffff3', //Ivory
   lightSecond: '#707070',
   dark: {
-    primary: '#0a0a0a', //Erie Black
-    accent: '#211d21', //Rasian Black
+    primary: '#0a0a0a', //Rich Black FOGRA 39 // RGB(10,10,10)
+    accent: '#211d21', //Rasian Black // RGB(33,29,33)
   },
-  accent: '#0D6E23', //Pakistan Green RGB(13,110,35)
+  accent: '#0D6E23', //Pakistan Green // RGB(13,110,35)
 }
 
 export default colors
