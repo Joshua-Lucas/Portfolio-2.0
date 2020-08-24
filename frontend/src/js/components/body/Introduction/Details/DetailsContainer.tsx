3e94e820564dcfa7c6ef53d9e8f16fe5eb346f59
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-24 19:06:24 -0400

    made dynamic and passed props

--- frontend/src/js/components/body/Introduction/Details/DetailsContainer.tsx
+++ frontend/src/js/components/body/Introduction/Details/DetailsContainer.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import DetailButtons from './DetailButtons'
 
+import {
+  Frontend,
+  Backend,
+  EssentailSkills,
+} from '../../../../utils/StaticData'
+
 //Styled Components
 const DetailsWrapper = styled.div`
-  padding-left: 100px;
+  padding-left: 12rem;
   grid-column: 1/2;
   grid-row: 2/3;
 
@@ -14,10 +20,16 @@ const DetailsWrapper = styled.div`
 
 //React Component
 const DetailsContainer: React.FC = () => {
-  return (
-    <DetailsWrapper>
-      <DetailButtons />
-    </DetailsWrapper>
-  )
+  const data = [Frontend, Backend, EssentailSkills]
+
+  const Buttons: any = data.map((object) => (
+    <DetailButtons
+      key={object.title}
+      title={object.title}
+      subtitle={object.definition}
+      accomplishments={object.accomplished}
+    />
+  ))
+  return <DetailsWrapper>{Buttons}</DetailsWrapper>
 }
 export default DetailsContainer
