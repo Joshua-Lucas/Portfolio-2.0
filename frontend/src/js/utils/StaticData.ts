ff492edaa1dd23b07766d13598eb81f9360703b4
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-24 19:04:49 -0400

    add more static data

--- frontend/src/js/utils/StaticData.ts
+++ frontend/src/js/utils/StaticData.ts
@@ -1,14 +1,17 @@
 export interface ISkills {
   title: string
+  definition: string
   subtitle: string
   sectionTitleOne: string
   sectionItemsOne: string[]
   sectionTitleTwo: string
   sectionItemsTwo: string[]
+  accomplished: string
 }
 
 export const Frontend = {
   title: 'Frontend',
+  definition: 'Creating amazining user experiences',
   subtitle:
     'I enjoy creating intuitive and responsive designs that offer a great user experience.',
   sectionTitleOne: 'Languages',
@@ -23,10 +26,12 @@ export const Frontend = {
     'Responsive Web Design',
     'DRY',
   ],
+  accomplished: '10 Projects',
 }
 
 export const Backend = {
   title: 'Backend',
+  definition: 'Building robust and scaleable architecture ',
   subtitle:
     "I strive to write clean and reusable code to create a scalable foundation to service the client's request.",
   sectionTitleOne: 'Languages',
@@ -43,10 +48,12 @@ export const Backend = {
     'TDD',
     'Solid Principles',
   ],
+  accomplished: '5 Projects',
 }
 
 export const EssentailSkills = {
   title: 'Essential Skills',
+  definition: 'Working with,learning from, and teaching others',
   subtitle:
     'I enjoy working with people, learning from them, and watching them develop in their skills.',
   sectionTitleOne: 'Opportunites I Draw From',
@@ -61,4 +68,5 @@ export const EssentailSkills = {
     'Friendly',
     'Buisness Minded',
   ],
+  accomplished: 'Over 8 Years Experience',
 }
