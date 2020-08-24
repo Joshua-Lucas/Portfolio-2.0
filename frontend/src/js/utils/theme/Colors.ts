2c94da39b19c35a5d9ccbd4a5a9a7e201adee81f
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-24 19:04:36 -0400

    updated green color

--- frontend/src/js/utils/theme/Colors.ts
+++ frontend/src/js/utils/theme/Colors.ts
@@ -5,7 +5,7 @@ const colors = {
     primary: '#0a0a0a', //Rich Black FOGRA 39 // RGB(10,10,10)
     accent: '#211d21', //Rasian Black // RGB(33,29,33)
   },
-  accent: '#0D6E23', //Pakistan Green // RGB(13,110,35)
+  accent: '#23BE53', // Green // RGBA(35, 190, 83)
 }
 
 export default colors
