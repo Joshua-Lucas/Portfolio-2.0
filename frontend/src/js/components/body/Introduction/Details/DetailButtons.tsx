d2bcdcb03a3c5fe7854f28edd0da6de4d071464b
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-27 19:01:13 -0400

    make a11y and implement eventHandler

--- frontend/src/js/components/body/Introduction/Details/DetailButtons.tsx
+++ frontend/src/js/components/body/Introduction/Details/DetailButtons.tsx
@@ -4,17 +4,23 @@ import SvgContainer from './SvgContainer'
 import { Icons, UtilityStyles } from '@jludev/component-lib-typescript'
 
 //Styled Components
-const DeatilButton = styled.div`
+const DeatilButton = styled.button`
   margin-bottom: 1rem;
   cursor: pointer;
   color: ${(props) => props.theme.primaryTextColor};
   background-color: ${(props) => props.theme.secondaryColor};
+  border: none;
   border-radius: ${UtilityStyles.borderRadius.lg};
+  text-align: left;
   display: flex;
 
   &:hover {
     box-shadow: 0 100px 100px 2px rgba(10, 10, 10, 0.5);
     z-index: 10;
+  }
+
+  &:focus {
+    border: 2px solid ${(props) => props.theme.lightTextColor};
   }
 `
 
@@ -30,13 +36,13 @@ const DetailTitle = styled.h1`
 `
 const DetailSubtitle = styled.p`
   color: ${(props) => props.theme.lightTextColor};
-  font-size: 0.75rem;
+  font-size: 0.85rem;
   font-weight: 200;
 `
 
 const DetailFootnote = styled.p`
   padding-top: 0.5rem;
-  font-size: 0.5rem;
+  font-size: 0.75rem;
 `
 
 // Interface
@@ -44,6 +50,7 @@ interface IDetailsButton {
   title: string
   subtitle: string
   accomplishments: string
+  toggle: React.MouseEventHandler<HTMLButtonElement>
 }
 
 //React Component
@@ -51,7 +58,9 @@ const DetailButtons: React.FC<IDetailsButton> = ({
   title,
   subtitle,
   accomplishments,
+  toggle,
 }) => {
+  //Methods
   const SvgIcons = {
     Frontend: Icons.Frontend,
     Backend: Icons.Server,
@@ -62,15 +71,16 @@ const DetailButtons: React.FC<IDetailsButton> = ({
     return IconsObject[IconTitle]
   }
 
+  const id = title.replace(' ', '')
+
   return (
-    <DeatilButton role="button">
+    <DeatilButton id={id} role="button" onClick={toggle}>
       <DetailsTitleContiner>
         <DetailTitle>{title}</DetailTitle>
         <DetailSubtitle>{subtitle}</DetailSubtitle>
         <DetailFootnote>{accomplishments}</DetailFootnote>
       </DetailsTitleContiner>
       <SvgContainer Paths={setSvgIcon(SvgIcons, title.replace(' ', ''))} />
-      {console.log(title.replace(' ', ''))}
     </DeatilButton>
   )
 }
